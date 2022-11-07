4c58919447f23221bd758daade595f209a7a870d
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-11-07 21:33:30 +0200

    Tehtävä 16

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,49 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_constructor(self):
+        players = self.statistics._players
+
+        self.assertEqual(len(players), 5)
+
+    def test_search_returns_player(self):
+        player = self.statistics.search("Kurri")
+
+        self.assertEqual(str(player), "Kurri EDM 37 + 53 = 90")
+
+    def test_search_returns_no_false_positives(self):
+        player = self.statistics.search("askndjas")
+
+        self.assertFalse(player)
+
+    def test_team_search(self):
+        team = self.statistics.team("EDM")
+
+        self.assertEqual(len(team), 3)
+
+    def test_top_three(self):
+        topThree = self.statistics.top(2)
+
+        self.assertEqual(len(topThree), 3)
+
+    def test_top_player(self):
+        best = self.statistics.top(1)
+
+        self.assertEqual(best[0].name, "Gretzky")
