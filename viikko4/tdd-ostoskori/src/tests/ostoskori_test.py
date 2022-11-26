bb7bb0b4f3ae52cd07b7004aa175fcb93ba48832
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-11-26 16:54:25 +0200

    Lisää testi kahden eri tuotteen hinnalle

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,4 +26,13 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(leipa)
 
-        self.assertEqual(self.kori.tavaroita_korissa(), 2) 
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_jalkeen_hinta_on_tuotteiden_summa(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+
+        self.assertEqual(self.kori.hinta(), 5)
+    
