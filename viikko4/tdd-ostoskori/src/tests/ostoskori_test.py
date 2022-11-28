3615376fdf613fc751c036fda077edc6775f2888
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-11-28 14:26:40 +0200

    Lisätty testi kahden saman tuotteen jälkeen korin ostoksella on sama nimi kuin tuotteella ja lukumäärä on kaksi + refaktorointi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -37,18 +37,16 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 5)
 
     def test_kahden_saman_tuotteen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito1 = Tuote("Maito", 3)
-        maito2 = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito1)
-        self.kori.lisaa_tuote(maito2)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
     
     def test_kahden_saman_tuotteen_jalkeen_korin_hinta_sama_kuin_tuotteen_hinta_tuplana(self):
-        maito1 = Tuote("Maito", 3)
-        maito2 = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito1)
-        self.kori.lisaa_tuote(maito2)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.hinta(), 6)
 
@@ -79,12 +77,23 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(len(ostokset), 2)
 
     def test_kahden_saman_tuotteen_jalkeen_korissa_yksi_ostos(self):
-        maito1 = Tuote("Maito", 3)
-        maito2 = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito1)
-        self.kori.lisaa_tuote(maito2)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
 
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_jalkeen_ostoksella_tuotteen_nimi_ja_oikea_lukumaara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+        ostos = ostokset[0]
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        self.assertEqual(ostos.tuotteen_nimi(), maito.nimi())
+
